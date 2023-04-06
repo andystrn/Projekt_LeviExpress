ea71bb7242091761235ac0c51c76505eb5b94a24
andystrn <an.strnadlova@gmail.com>
2023-04-06 19:00:38 +0200

    Routing

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
-);
+export const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/reservation',
+      element: <h2>Detail jízdenky</h2>
+    },
+  ])
+
+ return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+ )
+
+ 
+  
+};
